bc275dcdd33168bff07df40d56b724a1f144a0af
김보라 <bora.kim@odinue.net>
2019-02-23 16:29:05 +0900

    페이지방식

--- src/App.js
+++ src/App.js
@@ -1,67 +1,66 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// 선택 범위 : control-w
+/*const header = (
+  <header>
+    <h1 id={myTitleId}>ldk's {title}</h1>
+    <p>{desc}</p>
+  </header>
+);*/
 
 const Header = (props) => {
-  // console.log(props);
+  console.log(props);
   return (
     <header>
-      <h1>{ props.title }</h1>
-      <span className="stats">Players: { props.totalPlayers }</span>
+      <h1>{props.title}</h1>
+      <span className="stats">Players: {props.totalPlayers}</span>
     </header>
-  )
-};
+  );
+}
 
 class Counter extends React.Component {
-
   constructor(props) {
     super(props);
-
     this.state = {
+      players: 1,
       score: 0
     }
     // this.incrementScore.bind(this);
-  };
+  }
 
   incrementScore = () => {
-    // console.log('increment', this);  // lexical this : 자기 자신을 가르킨다.
-    // this.state.socre = 1;   // 이렇게 하면 값은 바뀔 순 있지만 화면이 렌더링이 되지 않는다.
-    // this.setState({score: 1});
-    this.setState(prevState => ({ score: prevState.score + 1 } ))
-
+    console.log('increment', this); // lexical this: 자기자신을 가르킨다
+    // this.state.score = 1; // 이렇게하면 화면이 렌더링이 안된다.
+    // this.setState({score: 1})
+    this.setState(prevState => ({score: prevState.score + 1}))
   };
 
-  decrementScore = () => {
-    this.setState(prevState => ({score: prevState.score - 1 }))
-  };
+  decrementSocre = () => {
+    this.setState(prevState => ({score: prevState.score - 1}))
+  }
 
   render() {
     return (
       <div className="counter">
-        <button className="counter-action decrement" onClick={this.decrementScore}> - </button>
+        <button className="counter-action decrement" onClick={this.decrementSocre}> - </button>
         <span className="counter-score">{this.state.score}</span>
         <button className="counter-action increment" onClick={this.incrementScore}> + </button>
       </div>
     );
   }
-};
+}
 
+const Player = (props) => (
+  <div className="player">
+    <span className="player-name">
+      <button className="remove-player" onClick={() => props.removePlayer(props.id)}>X</button>
+    </span>
+    <span className="player-name">{props.name}</span>
+    <Counter score={props.score}/>
+  </div>
+);
 
-const Player = (props) => {
-  return (
-    <div className="player">
-      <span className="player-name">
-        <button className="remove-player" onClick={() => props.removePlayer(props.id)}>x</button>
-      </span>
-      <span className="player-name">
-        {props.name}
-      </span>
-      <Counter />
-    </div>
-  );
-};
-
-// function component -> class component 로 변경
 class App extends React.Component {
   state = {
     players: [
@@ -70,34 +69,27 @@ class App extends React.Component {
       {name: 'KIM', id: 3},
       {name: 'PARK', id: 4},
     ]
-  };
+  }
 
   handleRemovePlayer = (id) => {
-    console.log(id);
-    this.setState(prevState => {
-      console.log(prevState.players);
-      return {
-        // players: prevState.players.filter(player => player.id !== id)
-        players: prevState.players.filter(player => player.id !== id)
-      }
-    })
-  };
-
+    this.setState(prevState => ({
+      players: prevState.players.filter(player => player.id !== id)
+    }))
+  }
 
   render() {
     return (
       <div className="scoreboard">
-        <Header title="My scoreboard" totalPlayers={this.state.players.length} />
+        <Header title="My scoreboard" totalPlayers={ 1 + 10 } />
 
-        {/*Players List*/}
-        { this.state.players.map(item =>
-          <Player
-            id={item.id.toString()}
-            name={item.name}
-            removePlayer={this.handleRemovePlayer}
-          />) }
+        {
+          this.state.players.map(play => <Player
+            id={play.id}
+            name={play.name}
+            score={play.score} key={play.id} removePlayer={this.handleRemovePlayer} />)
+        }
       </div>
-    );
+    )
   }
 }
 
